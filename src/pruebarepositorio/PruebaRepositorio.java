e5945abc7454e4a1a155def33c561acce1b6e8fc
jpmazate90 <juanpablo-orizabalgil@cunoc.edu.gt>
2018-07-24 22:53:10 -0600

    segundo aporte commit

--- src/pruebarepositorio/PruebaRepositorio.java
+++ src/pruebarepositorio/PruebaRepositorio.java
@@ -12,6 +12,7 @@ public class PruebaRepositorio {
      */
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("hola github");
     }
     
 }
