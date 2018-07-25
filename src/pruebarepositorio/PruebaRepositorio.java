f5ebdd3fbfa5e889213c64241efec3f16cc268dd
jpmazate90 <juanpablo-orizabalgil@cunoc.edu.gt>
2018-07-24 22:40:23 -0600

    mi primer commit

---
+++ src/pruebarepositorio/PruebaRepositorio.java
@@ -0,0 +1,17 @@
+
+package pruebarepositorio;
+
+/**
+ *
+ * @author jpmazate
+ */
+public class PruebaRepositorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+    
+}
